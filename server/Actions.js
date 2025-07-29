d71e1331f0fd08acf49b89865bf498bfc768b96c
Vivekscodes <viveks2026@mpgi.edu.in>
2025-07-29 11:25:38 +0530

    Minor changes

--- server/Actions.js
+++ server/Actions.js
@@ -1,10 +1,15 @@
-const ACTIONS = {
-    JOIN: 'join',
-    JOINED: 'joined',
-    DISCONNECTED: 'disconnected',
-    CODE_CHANGE: 'code-change',
-    SYNC_CODE: 'sync-code',
-    LEAVE: 'leave',
-}
+Here are some small improvements to the code:
 
-module.exports = ACTIONS;
+```javascript
+// Action constants for the code editor
+const ActionTypes = {
+    // User wants to join a session
+    JOIN: 'join',
+
+    // User has successfully joined a session
+    JOINED: 'joined',
+
+    // User has disconnected from the session
+    DISCONNECTED: 'disconnected',
+
+    // User has changed the local code
